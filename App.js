2c7447956dada8ced284369c5e9d606cdb1223f0
kaka <lluoyi.lu@gmail.com>
2020-05-31 14:39:25 +0800

    Updated app to use multiscreen
    Added react navigator packages

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import React from "react";
+import "react-native-gesture-handler";
 import { StyleSheet, Text, View } from "react-native";
-import Goban from "./goban/goban";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomNavigator } from "@react-navigation/bottom-tabs";
+import LoginScreen from "./screens/login";
+import MainScreen from "./screens/main";
+
+const Stack = createStackNavigator();
 
 export default class App extends React.Component {
   state = { boardWidth: 0 };
-
-  handlePress = () => {};
 
   measureView = (event) => {
     this.setState({ boardWidth: event.nativeEvent.layout.width });
@@ -13,12 +18,20 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <View
-        onLayout={(event) => this.measureView(event)}
-        style={styles.container}
-      >
-        <Goban boardWidth={this.state.boardWidth} boardSize={[13, 19]} />
-      </View>
+      <NavigationContainer>
+        <Stack.Navigator headerMode="none">
+          <Stack.Screen
+            name="Login"
+            component={LoginScreen}
+            options={{ title: "Welcome" }}
+          />
+          <Stack.Screen
+            name="Main"
+            component={MainScreen}
+            options={{ title: "Main Screen" }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     );
   }
 }
@@ -29,3 +42,12 @@ const styles = StyleSheet.create({
     flex: 1,
   },
 });
+
+/*
+<View
+          onLayout={(event) => this.measureView(event)}
+          style={styles.container}
+        >
+          <Goban boardWidth={this.state.boardWidth} boardSize={[9, 9]} />
+        </View>
+*/
